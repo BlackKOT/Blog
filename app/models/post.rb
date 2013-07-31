e387b4198c3c341b25453747fd024203023ff885
BlackKOT <oko10ko@ukr.net>
2013-07-31 16:43:09 +0300

    added CEO links and posts validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,11 @@
 class Post < ActiveRecord::Base
   attr_accessible :body, :title, :user_id
   belongs_to :user
+
+  validates :body, :length => { :in => 1..254 }, :allow_blank => false
+  validates :title, :presence => true
+
+  def to_param
+    "#{id}-#{title.parameterize}"
+  end
 end
