db02be331bfe1d832e47a9fd6482823886e04bec
BlackKOT <oko10ko@ukr.net>
2013-09-02 18:33:29 +0300

    added sql_clone_descendants_to

--- app/models/post.rb
+++ app/models/post.rb
@@ -24,4 +24,35 @@ class Post < ActiveRecord::Base
     end
   end
 
+  def sql_clone_descendants_to(target_node)
+
+    model = self.class.table_name
+    tree_model = "#{self.class.model_name.downcase}_hierarchies"
+    id_source_node = self.id
+
+    self.class.transaction do
+      sql_get_field_list = ActiveRecord::Base.connection().execute("
+        SELECT replace(array_to_string(ARRAY(SELECT coalesce(column_default,column_name)
+                                             FROM information_schema.columns
+                                             WHERE table_name = '#{model}'
+                                             ORDER BY ordinal_position
+        ), ','),'old_node','id')")
+
+      ActiveRecord::Base.connection.execute("
+        INSERT INTO #{model}
+          SELECT #{sql_get_field_list.first.values[0]} FROM #{model} t
+          JOIN #{tree_model} h ON (t.id = h.descendant_id)
+          WHERE h.ancestor_id = #{id_source_node} AND t.id != #{id_source_node};
+
+        UPDATE #{model} t
+        SET parent_id = (CASE parent_id
+                           WHEN #{id_source_node} THEN #{target_node.id}
+                           ELSE (SELECT id FROM #{model} t2
+                                 WHERE t2.old_node = t.parent_id)
+                         END),
+            old_node = NULL
+        WHERE old_node IS NOT NULL;")
+    end
+  end
+
 end
